fc330800639615e626b00abd7f3454cfbf4e4fda
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-05-07 23:16:24 +0300

    db service

---
+++ db.js
@@ -0,0 +1,10 @@
+const Currency = require('./models/currency.model')
+
+const saveCurrency = currency => new Currency(currency).save()
+// todo: modify to filter only sell and buy
+const getCurrencies = () => Currency.find({}).exec()
+
+module.exports = {
+	saveCurrency,
+	getCurrencies,
+}
