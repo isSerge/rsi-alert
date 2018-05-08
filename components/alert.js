6d338b84ae7c3ec56473801c9abbb924fb290871
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-05-08 21:14:23 +0300

    message from bot

--- components/alert.js
+++ components/alert.js
@@ -1,7 +1,14 @@
-const sendAlert = ({ name, rsi }) =>
-    rsi >= 70
-        ? console.log(name, 'SEND SELL NOTIFICATION', rsi)
-        : console.log(name, 'SEND BUY NOTIFICATION', rsi)
+const TelegramBot = require('node-telegram-bot-api')
+const { log } = require('../helpers')
+const token = process.env.TELEGRAM_TOKEN
+const bot = new TelegramBot(token, { polling: true })
+const chatId = process.env.CHAT_ID
+
+const sendAlert = ({ name, rsi }) => {
+    log({ name, rsi })
+    bot.sendMessage(chatId, `${rsi >= 70 ? 'sell' : 'buy'}: ${name}, ${rsi}`)
+    return console.log('Alert was sent')
+}
 
 module.exports = {
     sendAlert,
