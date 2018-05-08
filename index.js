93b3fb999d8ad8a3017e0bff4ed267ce506b1f71
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-05-08 15:27:17 +0300

    clean up

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const config = require('config')
 const mongoose = require('mongoose')
 const { handleCurrencies } = require('./components/rsi')
-const { saveCurrency, getCurrencies } = require('./components/db')
+const { getCurrencies } = require('./components/db')
 
 const interval = 15000
-
-// const currencies = [
-// 	{ name: 'eth' },
-// 	{ name: 'ada' },
-// 	{ name: 'neo', sell: true, buy: false },
-// 	{ name: 'bcc' },
-// 	{ name: 'xvg' },
-// 	{ name: 'xrp' },
-// 	{ name: 'ltc' },
-// 	{ name: 'omg' },
-// 	{ name: 'zec' },
-// 	{ name: 'xmr' },
-// 	{ name: 'zrx' },
-// 	{ name: 'dash' },
-// 	{ name: 'waves' },
-// 	{ name: 'eng' },
-// ]
 
 const executeJob = currencies => {
     handleCurrencies(currencies)
@@ -34,7 +17,6 @@ const db = mongoose.connection
 
 db.once('open', async () => {
     console.log('Database connection established')
-    // currencies.forEach(async x => await saveCurrency(x))
     const currencies = await getCurrencies()
     executeJob(currencies)
 })
