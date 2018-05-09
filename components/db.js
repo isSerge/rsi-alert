4cf754ef673a646e8e5a1ae4994888b4fc841c3e
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-05-09 20:42:26 +0300

    removeCurrency and updateCurrency

--- components/db.js
+++ components/db.js
@@ -1,13 +1,16 @@
 const R = require('ramda')
 const Currency = require('../models/currency.model')
 
-const saveCurrency = currency => new Currency(currency).save()
-// todo: modify to filter only sell and buy
 const getCurrencies = () => Currency.find({}).exec()
 const getCurrencyNames = async () => R.pluck('name', await getCurrencies())
+const removeCurrency = name => Currency.remove({ name }).exec()
+const saveCurrency = currency => new Currency(currency).save()
+const updateCurrency = ({ name, sell, buy }) => Currency.updateOne({ name }, { sell, buy })
 
 module.exports = {
     saveCurrency,
     getCurrencies,
     getCurrencyNames,
+    removeCurrency,
+    updateCurrency,
 }
