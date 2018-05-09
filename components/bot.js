6246d85d8c63d9a585539f493052d6991208aa89
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-05-09 20:20:38 +0300

    added /help, /ping and numbers for list of currencies

--- components/bot.js
+++ components/bot.js
@@ -3,6 +3,7 @@ const config = require('config')
 const TelegramBot = require('node-telegram-bot-api')
 const { logColored } = require('./logger')
 const { getCurrencyNames, saveCurrency } = require('./db')
+const { addNumbers } = require('../helpers')
 const token = process.env.TELEGRAM_TOKEN
 const bot = new TelegramBot(token, { polling: true })
 const chatId = process.env.CHAT_ID
@@ -18,14 +19,23 @@ const init = () => {
 
     bot.onText(/\/start/, msg => {
         const { id } = msg.from
-
         bot.sendMessage(id, config.get('messages.start') + config.get('messages.commands'))
+    })
+
+    bot.onText(/\/ping/, msg => {
+        const { id } = msg.from
+        bot.sendMessage(id, 'I am alive')
+    })
+
+    bot.onText(/\/help/, msg => {
+        const { id } = msg.from
+        bot.sendMessage(id, 'Commands and descriptions will be here')
     })
 
     bot.onText(/\/currencies/, async msg => {
         const { id } = msg.from
         const currencies = await getCurrencyNames()
-        bot.sendMessage(id, `selected currencies: ${currencies.join(', ')}`)
+        bot.sendMessage(id, `selected currencies: \n${addNumbers(currencies).join('\n')}`)
     })
 
     bot.onText(/\/add (.+)/, async (msg, match) => {
--- helpers.js
+++ helpers.js
@@ -1,7 +1,13 @@
+const R = require('ramda')
+
 const iterate = (handler, xs) => {
     for (const x of xs) handler(x)
 }
 
+const mapIndexed = R.addIndex(R.map)
+const addNumbers = mapIndexed((x, i) => `${i + 1}. ${x}`)
+
 module.exports = {
     iterate,
+    addNumbers,
 }
